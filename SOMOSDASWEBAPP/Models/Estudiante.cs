84b92e69d6278a876db539f1f2a6279522b00906
andersonsoria190s <anderson.soria@sistemas.edu.bo>
2023-12-17 19:02:31 -0400

    Prueba

---
+++ SOMOSDASWEBAPP/Models/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOMOSDASWEBAPP.Models
+{
+    public class Estudiante
+    {
+        [Key]
+        public int CedulaIdentidad { get; set; }
+        [Required]
+        public string? NombreCompleto { get; set; }
+        public int Celular { get; set; }
+        public string? CorreoElectronico { get; set; }
+
+        ///Relacion 
+
+        public virtual List<Inscripcion>? Inscripciones { get; set; }
+    }
+}
---
+++ SOMOSDASWEBAPP/wwwroot/js/crudC.js
@@ -0,0 +1,18 @@
+﻿document.getElementById("form-dur").addEventListener("input", verificarCampos);
+document.getElementById("form-tema").addEventListener("input", verificarCampos);
+document.getElementById("form-docente").addEventListener("input", verificarCampos);
+document.getElementById("form-cant").addEventListener("input", verificarCampos);
+function verificarCampos() {
+    var in1 = document.getElementById("form-dur").value;
+    var in2 = document.getElementById("form-tema").value;
+    var in3 = document.getElementById("form-docente").value;
+    var in4 = document.getElementById("form-cant").value;
+    var enlace = document.getElementById("enlace");
+
+    if (in1 !== "" && in2 !== "" && in3 !== "" && in4 !== "" ) {
+        enlace.style.display = "block"; // mostrar el enlace cuando ambos campos estén llenos
+    } else {
+        enlace.style.display = "none"; // ocultar el enlace si uno o ambos campos están vacíos
+    }
+}
+
